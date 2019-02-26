c96e10fd3e292f94bd9eb185038fd5bb2c4614b3
Andrew Twijukye <andrewbibangamba@gmail.com>
2019-02-26 12:32:23 +0300

    feat(connect recyclerview to mainactivity): Connect RecyclerView layout File to the MainActivity
    - add recyclyerview to mainactivity class and
    configure it with layout manager
    
    [Delivers #155768857]

--- app/src/main/java/com/levelup/bibangamba/githubusers/MainActivity.java
+++ app/src/main/java/com/levelup/bibangamba/githubusers/MainActivity.java
@@ -1,7 +1,9 @@
 package com.levelup.bibangamba.githubusers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView githubUsersRecyclerView = findViewById(R.id.recycler_view);
+        githubUsersRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        githubUsersRecyclerView.setLayoutManager(layoutManager);
     }
 }
