760d4cf1a9dd986d238e64f6907dd3430080abaa
Andrew Twijukye <andrewbibangamba@gmail.com>
2019-03-01 12:52:38 +0300

    feat(add adapter): Create the GithubUsersAdapter
    - add GithuAdpater in the adapter package
    - add Glide library to help load images using urls
    
    [Delivers #155965773]

---
+++ app/src/main/java/adapters/GithubUsersAdapter.java
@@ -0,0 +1,62 @@
+package adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.levelup.bibangamba.githubusers.R;
+
+import java.util.List;
+
+import model.GithubUsers;
+
+public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.GithubUserViewHolder> {
+    Context context;
+    private List<GithubUsers> githubUsers;
+
+    public GithubAdapter(Context context, List<GithubUsers> listOfGithubUsers) {
+        this.context = context;
+        this.githubUsers = listOfGithubUsers;
+    }
+
+
+    @NonNull
+    @Override
+    public GithubUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View githubUserListItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.github_user_list_item, parent, false);
+        return new GithubUserViewHolder(githubUserListItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GithubUserViewHolder githubUserViewHolder, int position) {
+        githubUserViewHolder.githubUsernameTextView.setText(githubUsers.get(position).getUsername());
+        Glide
+                .with(context)
+                .load(githubUsers.get(position).getProfilePicture())
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher)
+                .into(githubUserViewHolder.profilePictureImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return githubUsers.size();
+    }
+
+    static class GithubUserViewHolder extends RecyclerView.ViewHolder {
+        TextView githubUsernameTextView;
+        ImageView profilePictureImageView;
+
+        GithubUserViewHolder(View itemView) {
+            super(itemView);
+            this.githubUsernameTextView = itemView.findViewById(R.id.githubUsernameTextView);
+            this.profilePictureImageView = itemView.findViewById(R.id.profilePictureImageView);
+        }
+    }
+}
